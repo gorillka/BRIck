1e80080f62a8102a1a245d06535e2209cdb2d245
Artem Orynko <a.orynko@gmail.com>
2018-07-31 21:27:02 +0300

    Init project

---
+++ BRIck/BRIck.h
@@ -0,0 +1,19 @@
+//
+//  BRIck.h
+//  BRIck
+//
+//  Created by Gorilka on 7/31/18.
+//  Copyright © 2018 Gorilka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BRIck.
+FOUNDATION_EXPORT double BRIckVersionNumber;
+
+//! Project version string for BRIck.
+FOUNDATION_EXPORT const unsigned char BRIckVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BRIck/PublicHeader.h>
+
+
