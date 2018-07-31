7f10843e02c750e056d30e94e2726c11aab38e81
Artem Orynko <a.orynko@gmail.com>
2018-07-31 21:29:43 +0300

    add dependency protocols

---
+++ BRIck/Classes/DI/Dependency.swift
@@ -0,0 +1,10 @@
+//
+//  Dependency.swift
+//  BRIck
+//
+//  Created by Gorilka on 7/31/18.
+//  Copyright © 2018 Gorilka. All rights reserved.
+//
+
+public protocol Dependency: class {}
+public protocol EmptyDependency: Dependency {}
