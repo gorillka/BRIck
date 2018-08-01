0d1b48730a90381390175b384a3fb293dc1653f1
Artem Orynko <a.orynko@gmail.com>
2018-08-01 13:36:09 +0300

    add implementation for interactor

--- BRIck/Classes/Interactor.swift
+++ BRIck/Classes/Interactor.swift
@@ -8,4 +8,56 @@
 
 import Foundation
 
-public protocol Interactable {}
+public protocol InteractorScope: class {
+    typealias ActivityHandler = (Bool) -> Swift.Void
+    var isActive: Bool { get }
+    var activityHandler: ActivityHandler? { get set }
+}
+
+public protocol Interactable: InteractorScope {
+    func activate()
+    func deactivate()
+}
+
+open class Interactor: Interactable {
+    public var activityHandler: ActivityHandler?
+
+    public private(set) var isActive: Bool = false {
+        didSet {
+            activityHandler?(isActive)
+        }
+    }
+
+    public init() {}
+
+    open func didBecomeActive() {}
+    open func willResignActive() {}
+
+    public func activate() {
+        guard !isActive else {
+            return
+        }
+
+        isActive = true
+
+        didBecomeActive()
+    }
+
+    public func deactivate() {
+        guard isActive else {
+            return
+        }
+
+        willResignActive()
+
+        isActive = false
+    }
+
+    deinit {
+        if isActive {
+            deactivate()
+        }
+
+        activityHandler = nil
+    }
+}
