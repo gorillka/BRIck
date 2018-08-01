ccc05250ae72117fe9ba570e2d1089ea3ed6fdb8
Artem Orynko <a.orynko@gmail.com>
2018-08-01 12:50:31 +0300

    add router

---
+++ BRIck/Classes/Router.swift
@@ -0,0 +1,11 @@
+//
+//  Router.swift
+//  BRIck
+//
+//  Created by Gorilka on 7/31/18.
+//  Copyright © 2018 Gorilka. All rights reserved.
+//
+
+import Foundation
+
+public protocol Routing: class {}
