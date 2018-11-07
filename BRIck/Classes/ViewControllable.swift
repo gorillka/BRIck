e6cd6a79d4b9abc89f65f189d34238a2dc243524
Artem Orynko <artem.or@onixcat.com>
2018-11-07 17:18:30 +0200

    minorfix

--- BRIck/Classes/ViewControllable.swift
+++ BRIck/Classes/ViewControllable.swift
@@ -14,7 +14,7 @@ public protocol ViewControllable: class {
 }
 
 /// Default implementation on `UIViewController` to conform to `ViewControllable` protocol.
-public extension ViewControllable where Self: UIViewController {
+public extension ViewControllable {
     var uiViewController: UIViewController {
         return self
     }
