971f79de11d9309cbe9464a5835ede6ceedf65ef
Artem Orynko <artem.or@onixcat.com>
2018-12-02 08:59:09 +0200

    update tooling
    add template for component extension
    add template for unit tests

---
+++ tooling/BRIck Unit Tests.xctemplate/___FILEBASENAME___Interactor.swift
@@ -0,0 +1,20 @@
+// ___FILEHEADER___
+
+@testable import ___PROJECTNAME___
+import XCTest
+
+final class ___VARIABLE_productName___InteractorTests: XCTestCase {
+
+    private var interactor: ___VARIABLE_productName___Interactor!
+
+    // TODO: declare other objects and mocks you need as provate vars
+
+    override func setUp() {
+        super.setUp()
+
+        // TODO: instantiate objects and mocks
+    }
+
+    // MARK: - Tests
+
+}
