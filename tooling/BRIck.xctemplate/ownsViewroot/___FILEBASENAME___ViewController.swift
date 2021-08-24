7129de64d7b23ec1dba986b596429c1b94d0b520
Artem Orynko <a.orynko@gmail.com>
2021-08-24 14:43:13 +0300

    update tooling

--- tooling/BRIck.xctemplate/ownsViewroot/___FILEBASENAME___ViewController.swift
+++ tooling/BRIck.xctemplate/ownsViewroot/___FILEBASENAME___ViewController.swift
@@ -3,7 +3,7 @@
 import UIKit
 import BRIck
 
-protocol ___VARIABLE_productName___PresentableListener: class {
+protocol ___VARIABLE_productName___PresentableListener: AnyObject {
 
     // TODO: Declare properties and methods that the view controller can invoke to perform business logic, such as signIn().
     // This protocol is implemented by the corresponding interactor class.
